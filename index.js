d3b38aa3b91ae67d0fe051bad2f6ffe5299e2b56
Bryan Alcantara <bryanalcantara@Bryans-MacBook-Air.local>
2024-03-16 11:23:51 -0400

    forms with events

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Code your solution!")
+let button = document.querySelector('.add-button');
+let form = document.querySelector("form");
+let li = document.createElement("li");
+let ul = document.querySelector("ul");
+
+button.addEventListener("click",event =>
+    {
+    event.preventDefault();
+    let text = document.getElementById('task1').value;
+    let li = document.createElement("li");
+    let createButton = document.createElement("button");
+    createButton.setAttribute("class","delete-button");
+    createButton.innerText="delete";
+    li.innerText = text;
+    ul.append(li);
+    li.append(createButton);
+    
+
+})
